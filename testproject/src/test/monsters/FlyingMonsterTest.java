a63cf5045b1b1f804d9d0efc513337ae3c319d3e
mr-disgrace <nichgrace33@gmail.com>
2022-05-20 22:15:27 +1200

    Added unit tests for all Monster subclasses

--- testproject/src/test/monsters/FlyingMonsterTest.java
+++ testproject/src/test/monsters/FlyingMonsterTest.java
@@ -4,11 +4,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import game.monsters.FlyingMonster;
+
 class FlyingMonsterTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void regularAttackTest() {
+		//test that subclass monsters can still do regular attack.
+		FlyingMonster fly = new FlyingMonster("fly", 1);
+		FlyingMonster enemy = new FlyingMonster("enemy", 1);
+		fly.makeMove(0, enemy);
+		assertTrue(enemy.getHealth() < enemy.getMaxHealth());
+	}
+	
+	@Test
+	void flyTest() {
+		FlyingMonster fly = new FlyingMonster("fly", 1);
+		fly.dealDamageToSelf(1);
+		assertEquals(fly.getHealth(), fly.getMaxHealth() - 1);
+		fly.makeMove(1, null);
+		fly.dealDamageToSelf(1);
+		assertEquals(fly.getHealth(), fly.getMaxHealth() - 1);
+		fly.preTurnLogic();
+		fly.dealDamageToSelf(1);
+		assertEquals(fly.getHealth(), fly.getMaxHealth() - 2);
+		fly.makeMove(1, null);
+		fly.rest();
+		fly.dealDamageToSelf(1);
+		assertEquals(fly.getHealth(), fly.getMaxHealth() - 1);
 	}
 
 }
--- testproject/src/test/monsters/PoisonMonsterTest.java
+++ testproject/src/test/monsters/PoisonMonsterTest.java
@@ -4,11 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import game.monsters.PoisonMonster;
+
 class PoisonMonsterTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void poisonTest() {
+		PoisonMonster poison = new PoisonMonster("poison", 1);
+		PoisonMonster enemy = new PoisonMonster("enemy", 5);//high level so have lots of health and won't drain to 0 over test.
+		int lastHealth = enemy.getHealth();
+		poison.makeMove(1, enemy);
+		assertTrue(enemy.getHealth() < lastHealth);
+		lastHealth = enemy.getHealth();
+		enemy.preTurnLogic();
+		assertTrue(enemy.getHealth() < lastHealth);
+		lastHealth = enemy.getHealth();
+		for(int i=0; i<4; i++) enemy.preTurnLogic();
+		assertTrue(enemy.getHealth() < lastHealth);
+		lastHealth = enemy.getHealth();
+		enemy.preTurnLogic();
+		assertFalse(enemy.getHealth() < lastHealth);
 	}
 
 }
