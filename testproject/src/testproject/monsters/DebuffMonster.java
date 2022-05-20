7b861d4510203364c3df3cae48c072fc17541931
mr-disgrace <nichgrace33@gmail.com>
2022-05-20 15:38:27 +1200

    Added documentation for Monster subclasses.

--- testproject/src/testproject/monsters/DebuffMonster.java
+++ testproject/src/testproject/monsters/DebuffMonster.java
@@ -2,23 +2,44 @@ package testproject.monsters;
 
 import java.util.Random;
 
+/**
+ * A special type of Monster, with a second attack which applies a debuff to the enemies damage.
+ */
 public class DebuffMonster extends Monster {
+	/**
+	 * Used for name generation.
+	 */
 	private static String[] types = {"Hyena", "Panda"};
 	
+	/**
+	 * Calls the Monster constructor, and then adds the attack string of the debuff attack.
+	 * @param name The monsters name.
+	 * @param startLevel The monster's starting level.
+	 */
 	public DebuffMonster(String name, int startLevel) {
 		super(name, startLevel);
-		attacks.add("1: Debuff attack.");
+		attacks.add("Debuff attack.");
 	}
 	
+	/**
+	 * Generates a random name for this type of monster, using the base Monster descriptives and the DebuffMonster types strings.
+	 * @return A random name for a DebufMonster
+	 */
 	public static String getRandomName() {
 		Random rand = new Random();
 		return descriptives[rand.nextInt(descriptives.length)] + " " + types[rand.nextInt(types.length)];
 	}
 	
+	/**
+	 * Performs debuff attack if move is 1, else calls base Monster makeMove().
+	 */
 	public String makeMove(int move, Monster enemy) {
 		if(move == 1) return debuff(enemy);
 		return super.makeMove(move, enemy);
 	}
+	/**
+	 * 50% chance to make a debuff attack, else makes a basic attack.
+	 */
 	public String makeRandomMove(Monster enemy) {
 		Random rand = new Random();
 		int move=rand.nextInt(2);
@@ -26,6 +47,12 @@ public class DebuffMonster extends Monster {
 		else return super.makeRandomMove(enemy);
 	}
 	
+	
+	/**
+	 * Applies a debuff to the enemy for 3 turns of a third of this monster's attack damage.
+	 * @param enemy The enemy Monster to apply the debuff to.
+	 * @return A string describing the debuff.
+	 */
 	private String debuff(Monster enemy) {
 		enemy.addDamageBuff(-getTotalDamage()/3, 3);
 		return "Debuffed " + enemy.getName() + " by " + getTotalDamage()/3;
--- testproject/src/testproject/monsters/PoisonMonster.java
+++ testproject/src/testproject/monsters/PoisonMonster.java
@@ -2,24 +2,44 @@ package testproject.monsters;
 
 import java.util.Random;
 
+/**
+ * A special type of Monster with a poison attack.
+ */
 public class PoisonMonster extends Monster {
+	/**
+	 * Used for name generation.
+	 */
 	private static String[] types = {"Frog", "Snake"};
 	
-	
+	/**
+	 * Calls base Monster constructor and adds attack string for poison attack.
+	 * @param name The mosnter's name.
+	 * @param startLevel The monster's start level.
+	 */
 	public PoisonMonster(String name, int startLevel) {
 		super(name, startLevel);
-		attacks.add("1: Poison attack.");
+		attacks.add("Poison attack.");
 	}
 	
+	/**
+	 * Generates a random name for PoisonMonster
+	 * @return A random name for PoisonMonster
+	 */
 	public static String getRandomName() {
 		Random rand = new Random();
 		return descriptives[rand.nextInt(descriptives.length)] + " " + types[rand.nextInt(types.length)];
 	}
 	
+	/**
+	 * If move is 1 makes poison attack, else makes base class move.
+	 */
 	public String makeMove(int move, Monster enemy) {
 		if(move == 1) return poison(enemy);
 		return super.makeMove(move, enemy);
 	}
+	/**
+	 * Randomly makes poison attack or base class move with 50% probability of each.
+	 */
 	public String makeRandomMove(Monster enemy) {
 		Random rand = new Random();
 		int move=rand.nextInt(2);
@@ -27,6 +47,12 @@ public class PoisonMonster extends Monster {
 		else return super.makeRandomMove(enemy);
 	}
 	
+	/**
+	 * The poison attack, dealing half this monster's attack damage to the enemy, and then adding a persistent damage attack of 
+	 * one fifth of this monster's total damage, which lasts for five turns.
+	 * @param enemy The enemy Monster.
+	 * @return A String describing the attack.
+	 */
 	private String poison(Monster enemy) {
 		String ret = "";
 		ret += enemy.dealDamageToSelf(getTotalDamage()/2) + "\n";
